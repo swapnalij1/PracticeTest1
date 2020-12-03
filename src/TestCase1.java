c9f53dd3003d8695c88d523cca0603c5c57b2abf
SwapnaliJ <swapnalij4050@gmail.com>
2020-12-03 12:56:08 +0530

    firstcommit

---
+++ src/TestCase1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in= new Scanner(System.in);
+		int j=in.nextInt();
+		
+	if(j>5)
+	
+	{
+		System.out.println("Hello");
+	}
+	else System.out.println("Bye");
+
+	
+	}
+
+}
